06f36003105c7d73ea73684f593913a0826c4a3e
musa-radebe233 <102368031+musa-radebe233@users.noreply.github.com>
2022-04-14 09:24:14 +0200

    Delete task 0.7.py

--- task 0.7.py
+++
@@ -1,8 +0,0 @@
-# Task 0.7
-celsius = float(35)
-fahrenheit = (celsius * 9/5) + 32
-print("%f fahrenheit" %(fahrenheit))
-
-fahrenheit = float(85)
-celsius = (fahrenheit - 32) * 5/9
-print("%f celsius" %(celsius))
