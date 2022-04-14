3cb5fb91f612660ae62397b8a5e382c0769733f5
musa-radebe233 <102368031+musa-radebe233@users.noreply.github.com>
2022-04-14 09:24:24 +0200

    Delete task 0.8.py

--- task 0.8.py
+++
@@ -1,6 +0,0 @@
-# Task 0.8
-time = float(71)
-hours = int(time / 60)
-hours_and_minutes = float(time / 60)
-minutes =(hours_and_minutes - hours) * 60
-print("%.2f hours %0.2f minutes" %(hours, minutes))
