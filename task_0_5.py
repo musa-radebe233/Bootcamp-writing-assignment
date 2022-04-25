40ad61c80561a8c26d7b1e65683b9344d18d3454
musa-radebe233 <102368031+musa-radebe233@users.noreply.github.com>
2022-04-25 08:15:29 +0200

    Update task_0_5.py

--- task_0_5.py
+++ task_0_5.py
@@ -1,6 +1,6 @@
-def area(a, b, c,):
-    semi_perimeter = 1/2 * (a + b + c)
-    area = (semi_perimeter * ((semi_perimeter - a)*(semi_perimeter - b)*(semi_perimeter - c))) **0.5
+def area(length1, length2, length3,):
+    semi_perimeter = 1/2 * (length1 + length2 + length3)
+    area = (semi_perimeter * ((semi_perimeter - length1)*(semi_perimeter - length2)*(semi_perimeter -length3))) **0.5
     return area
 print(area(3, 4, 5,))
     
