ba43afa73451029437e230a9825e2ee9672ef9e5
musa-radebe233 <102368031+musa-radebe233@users.noreply.github.com>
2022-05-08 20:06:48 +0200

    Update task_0_9.py

--- task_0_9.py
+++ task_0_9.py
@@ -3,9 +3,6 @@ def vowel(text):
     d = ""
     print("Vowels:", end = '')
     for i in text:
-        if i not in d:
-            d = d + i
-    for i in d:
         if i in vowels and i != d.upper():
             print(i, end = ' ,')
 vowel(list("Umuzi"))
