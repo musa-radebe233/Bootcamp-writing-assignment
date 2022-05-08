7408d7f8daca4cb3db404bc52ce09ab845b72650
musa-radebe233 <102368031+musa-radebe233@users.noreply.github.com>
2022-05-08 20:08:26 +0200

    Update task_0_10.py

--- task_0_10.py
+++ task_0_10.py
@@ -2,8 +2,9 @@ def common_letters(var1, var2):
    s1 = set(var1)
    s2 = set(var2)
    words = list(s1 & s2)
+   print("Common letters:")
    for i in words:
-      print(i, end = ' ,')  
+      print(i, end = ' ')  
 common_letters("house","mouse")
 
 
