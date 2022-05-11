8deb5792734f5029cdba1a984bc19e6dac91b5ab
musa-radebe233 <102368031+musa-radebe233@users.noreply.github.com>
2022-05-11 13:54:02 +0200

    Update task_0_8.py

--- task_0_8.py
+++ task_0_8.py
@@ -4,10 +4,14 @@ def convert_to_min(time):
     minute = (hours_and_minutes - hour) * 60
     if hour > 1 and minute > 1:
         print("%01d hours, %01d minutes" % (hour, minute))
-    elif hour <= 1 and minute <= 1:
-        print("%01d hour, %01d minute" % (hour, minute))
-    elif hour <= 1 and minute > 1:
-        print("%01d hour, %01d minutes" % (hour, minute))
-    elif hour > 1 and minute <= 1:
+    elif hour < 1 and minute == 1:
         print("%01d hours, %01d minute" % (hour, minute))
+    elif hour == 1 and minute > 1:
+        print("%01d hour, %01d minutes" % (hour, minute))
+    elif hour > 1 and minute == 1:
+        print("%01d hours, %01d minute" % (hour, minute))
+    elif hour > 1 and minute < 1:
+        print("%01d hours, %01d minutes" % (hour, minute))
+    elif hour == 1 and minute < 1:
+        print("%01d hour, %01d minutes" % (hour, minute))
 convert_to_min(133)
