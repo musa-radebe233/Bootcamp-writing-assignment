1be9ffab43a8c146cf3e03ff0ef68fb3d5eb0a7e
musa-radebe233 <102368031+musa-radebe233@users.noreply.github.com>
2022-05-12 09:32:45 +0200

    Update task_0_10.py

--- task_0_10.py
+++ task_0_10.py
@@ -2,6 +2,7 @@ def common_letters(var1, var2):
    s1 = set(var1)
    s2 = set(var2)
    words = list(s1 & s2)
+   empty_string = ""
    print("Common letters:",end = ' ')
    for i in words:
       if i in words and i != empty_string.upper():
