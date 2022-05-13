de65346d5ade6b2dc95a490b22f89d5a2fabe2e3
musa-radebe233 <102368031+musa-radebe233@users.noreply.github.com>
2022-05-13 08:24:32 +0200

    Update task_0_9.py

--- task_0_9.py
+++ task_0_9.py
@@ -1,7 +1,7 @@
 def vowel(text):
    vowels = "aeiou"
-    words = set(vowel for vowel in text.lower() if vowel in vowels)
-    letters = ",".join(words)
+    words = set((vowel for vowel in text.lower() if vowel in vowels))
+    letters = ", ".join(words)
     print(f" Vowels: {letters}")
 vowel("Umuzi")
 
